ec314cff1a2ff103fb95b5cbea002c3eca117463
David Adams <davidadams@Davids-MacBook-Pro.local>
2018-01-23 16:28:28 -0800

    adds axios

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,16 +1,61 @@
 <template>
   <div class="work">
-    <h1>{{ msg }}</h1>
+
+    <div class="loading" v-if="loading">
+      <img src="../assets/loading.gif" alt="loading...">
+    </div>
+
+    <div v-if="error" class="error">
+      {{error}}
+    </div>
+
+    <div v-if="work" v-for="i in work" class="content">
+      <h2>{{i.name}}</h2>
+      <p>{{i.desc}}</p>
+    </div>
+
   </div>
 </template>
 
+
+
+
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Work',
-  data () {
+
+  data() {
     return {
-      msg: 'WORK'
+      loading: false,
+      work: null,
+      error: null,
     }
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  watch: {
+    '$route': 'fetchData'
+  },
+
+  methods: {
+    fetchData() {
+      this.loading = true;
+
+      axios.get('https://david-adams-resume.firebaseio.com/work.json')
+      .then((resp) => {
+        this.work = resp.data
+        this.loading = false;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+    
   }
 }
 </script>
