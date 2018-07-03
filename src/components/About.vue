f32b7d6249be90de1cccab2ea0febfb452939048
soundslikebliss <russianid1@gmail.com>
2018-07-03 13:00:16 -0700

    loader tweak

--- src/components/About.vue
+++ src/components/About.vue
@@ -41,7 +41,7 @@ export default {
   data () {
     return {
       title: 'About',
-      loading: true,
+      loading: false,
       data: '',
       error: ''
     }
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -16,7 +16,7 @@ export default {
 
   data () {
     return {
-      loading: true,
+      loading: false,
       data: '',
       error: ''
     }
